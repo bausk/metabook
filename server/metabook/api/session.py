322d4f1b9d3e325a9f6a9c75135c53bf405bbf8a
bausk <bauskas@gmail.com>
2016-02-11 21:58:34 +0200

    reordering

--- server/metabook/api/session.py
+++ server/metabook/api/session.py
@@ -1,7 +1,9 @@
 import tornado.web
 import tornado.gen
+from metabook.handlers import APIHandler
 
-class StartHandler(tornado.web.RequestHandler):
+
+class SessionHandler(APIHandler):
     @tornado.gen.coroutine
     def get(self):
         self.render("index.html")
--- server/server.py
+++ server/server.py
@@ -1,3 +1,11 @@
+# server.py
+# (c) 2016 Alexander Bausk
+
+# Sources:
+# https://github.com/hfaran/CitySportsLeague-Server/blob/master/src/app.py
+# http://stackoverflow.com/questions/14385048/is-there-a-better-way-to-handle-index-html-with-tornado
+# https://github.com/zhanglongqi/TornadoAJAXSample
+
 import logging
 import os
 import time
@@ -16,9 +24,36 @@ from tornado_json.routes import get_routes
 import metabook.api
 from metabook.config import ServerConfig
 
+def sig_handler(sig, frame):
+    """Handles SIGINT by calling shutdown()"""
+    logging.warning('Caught signal: %s', sig)
+    tornado.ioloop.IOLoop.instance().add_callback(shutdown)
+
+def shutdown():
+    """Waits MAX_WAIT_SECONDS_BEFORE_SHUTDOWN, then shuts down the server"""
+    MAX_WAIT_SECONDS_BEFORE_SHUTDOWN = 1
+
+    logging.info('Stopping http server')
+    http_server.stop()
+
+    logging.info('Will shutdown in %s seconds ...',
+                 MAX_WAIT_SECONDS_BEFORE_SHUTDOWN)
+    io_loop = tornado.ioloop.IOLoop.instance()
+
+    deadline = time.time() + MAX_WAIT_SECONDS_BEFORE_SHUTDOWN
+
+    def stop_loop():
+        now = time.time()
+        if now < deadline and (io_loop._callbacks or io_loop._timeouts):
+            io_loop.add_timeout(now + 1, stop_loop)
+        else:
+            io_loop.stop()
+            logging.info('Shutdown')
+
+    stop_loop()
 
 @click.command()
-@click.option('-p', '--port', default=8888, type=int, required=True,
+@click.option('-p', '--port', default=8585, type=int, required=True,
               help="Port to start server on")
 @click.option('--debug', is_flag=True)
 def main(port, debug):
@@ -29,32 +64,23 @@ def main(port, debug):
         debug=debug
     )
 
-
     settings = dict(
         template_path=os.path.join(
             os.path.dirname(__file__), "templates"),
         static_path=os.path.join(os.path.dirname(__file__), "static"),
         gzip=True,
-        cookie_secret=(cookie_secret if cookie_secret
-                       else uuid.uuid4().hex),
-        app_config=app_config,
-        login_url="/api/auth/playerlogin"
+        app_config=app_config
     )
 
     # Create server
     http_server = tornado.httpserver.HTTPServer(
         Application(
-            routes=get_routes(wlsports.api),
-            settings=settings,
-            db_conn=wlsports.db,
+            routes=get_routes(metabook.api),
+            settings=settings
         )
     )
     # Bind to port
     http_server.listen(port)
-
-    # Register signal handlers for quitting
-    signal.signal(signal.SIGTERM, sig_handler)
-    signal.signal(signal.SIGINT, sig_handler)
 
     # Start IO loop
     tornado.ioloop.IOLoop.instance().start()
@@ -63,5 +89,3 @@ def main(port, debug):
 
 if __name__ == "__main__":
     main()
-
-inspect.getargs()
